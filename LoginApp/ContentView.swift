2e86065b78e957bfbc54a82ddc25c53002afbffe
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2023-10-10 04:03:02 +0600

    Update ContentView.swift

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -19,11 +19,6 @@ struct Home: View {
         GeometryReader {_ in
             ScrollView {
                 VStack {
-                    // Logo
-                    Image("logo")
-                        .resizable()
-                        .frame(width: 60, height: 60, alignment: .center)
-                    
                     // Login View
                     Login(index: $index)
                     
